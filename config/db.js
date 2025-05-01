269c97a7b5c358cb1214f4fd9fd6a872fa053307
Darshang-04 <gowdadarshan694@gmail.com>
2025-05-01 12:40:38 +0530

    updated commit

--- config/db.js
+++ config/db.js
@@ -1,8 +1,14 @@
-const {Sequelize} = require('sequelize')
+const { Sequelize } = require('sequelize');
+require('dotenv').config(); // Load env vars
 
-const sequelize = new Sequelize('Bank', 'root', 'root@123',{
-    host:'localhost',
-    dialect:'mysql'
-})
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || 'mysql',
+  }
+);
 
 module.exports = sequelize;
